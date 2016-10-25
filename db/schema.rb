d9960aecc53c9b1c5f5487d6fff88901950ac661
teddycoleman <teddy.f.coleman@gmail.com>
2016-10-25 10:37:33 -0700

    adds migration fix for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025172452) do
+ActiveRecord::Schema.define(version: 20161025173544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,13 +24,13 @@ ActiveRecord::Schema.define(version: 20161025172452) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.integer  "users_id"
+    t.integer  "user_id"
     t.text     "post_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "city_id"
     t.index ["city_id"], name: "index_posts_on_city_id", using: :btree
-    t.index ["users_id"], name: "index_posts_on_users_id", using: :btree
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,5 +49,5 @@ ActiveRecord::Schema.define(version: 20161025172452) do
   end
 
   add_foreign_key "posts", "cities"
-  add_foreign_key "posts", "users", column: "users_id"
+  add_foreign_key "posts", "users"
 end
