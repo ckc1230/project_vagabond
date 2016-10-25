a6d990fd9ded53f81f0f7c5b46ea4d67caddc8b2
ckc1230 <chriskchan1230@gmail.com>
2016-10-25 09:38:30 -0700

    added paperclip and imagemagick - added migration for :photo on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:first_name, :last_name, :current_city, :email, :profile_image, :password)
+		params.require(:user).permit(:first_name, :last_name, :current_city, :email, :profile_image, :password, :photo)
 	end
 
 end
