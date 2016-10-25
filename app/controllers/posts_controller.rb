e7f2e2b0845390a6784f8bfb3644cd3e3de41bd7
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-10-25 14:32:18 -0700

    adds new post modal and post create functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,29 @@
 class PostsController < ApplicationController
 	
+	def new
+	end
+
+	def create
+		post = current_user
+						.posts
+						.create(post_params)
+
+		if post.valid?
+        redirect_to current_user
+    else
+        flash[:error] = "post didn't save correctly"
+        redirect_to :back
+    end
+  end
+
 	def show
 		@user = User.find(params[:id])
+	end
+
+	private
+
+	def post_params
+			params.require(:post).permit(:title, :post_text)
 	end
 
 end
