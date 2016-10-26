f7f62df85f76038cd307cbbc99dede9c69c4dda2
teddycoleman <teddy.f.coleman@gmail.com>
2016-10-26 09:27:52 -0700

    Update posts_helper.rb

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,3 @@
 module PostsHelper
-	# def truncate(text, options = {}, &block)
- #  	if text
- #    length  = options.fetch(:length, 2)
 
- #    content = text.truncate(length, options)
- #    content = options[:escape] == false ? content.html_safe : ERB::Util.html_escape(content)
- #    content << capture(&block) if block_given? && text.length > length
- #    content
- #  	end
-	# end
 end
