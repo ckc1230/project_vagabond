7132003fd6a9fc3f1d1ff8960e0b365b8f1125a3
ckc1230 <chriskchan1230@gmail.com>
2016-10-26 14:38:29 -0700

    added pagination, user show page contains cities posted in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
  
 	def show
 		@user = User.friendly.find(params[:id])
+		@posts = @user.posts.paginate(:page => params[:page], :per_page => 3)
+		posted_cities_id = get_cities_id
+		@posted_cities = get_cities(posted_cities_id)
 	end
 
 	def new
@@ -19,9 +22,23 @@ class UsersController < ApplicationController
 		 redirect_to user_path
 	end
 
+	def get_cities_id
+		cities_id = [].to_set
+		@user.posts.each do |f|
+			cities_id.add(f.city_id)
+		end
+		return cities_id
+	end
+	def get_cities(id_array)
+		cities = [].to_set
+		id_array.each do |f|
+			cities.add(City.find(f))
+		end
+		return cities
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:first_name, :last_name, :current_city, :email, :profile_image, :password, :photo)
 	end
-
 end
