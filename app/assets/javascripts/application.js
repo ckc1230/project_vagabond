9caa1f59cdcfda86f66a30db3c34b7a2f7324094
ckc1230 <chriskchan1230@gmail.com>
2016-10-27 09:38:09 -0700

    commit to prep for merge: not a clean commit to jump back to

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,51 +38,101 @@ $( document ).on("turbolinks:load",function() {
 	  $(this).closest('.user-post').find('.post-footer').toggle();
 	})
 
-  $('.back-arrow').on('click', scrollBackThroughImages);
-  $('.forward-arrow').on('click', scrollForwardThroughImages);
+  // $('.back-arrow').on('click', scrollBackThroughImages);
+  // $('.forward-arrow').on('click', scrollForwardThroughImages);
+
+  $('.back-arrow').on('click', scrollBackThroughDisplays);
+  $('.forward-arrow').on('click', scrollForwardThroughDisplays);
 
   $('#new_user').on('submit', signUp);
   $('#login_form').on('submit', signIn);
   $('#new_post').on('submit', newPost);
 
-  hideImagesExceptFirstOne();
+  hideDisplaysExceptFirstOne();  
+  // hideImagesExceptFirstOne();
 });
 
-var photoIndex = 0 
-function scrollBackThroughImages(){
-	var photoArray = $('.cover-photo');
-	$(photoArray[photoIndex]).toggle();
-	if(photoIndex > 0){
-		photoIndex--;	
-	}
+// var photoIndex = 0 
+// function scrollBackThroughImages(){
+// 	var photoArray = $('.cover-photo');
+// 	$(photoArray[photoIndex]).toggle();
+// 	if(photoIndex > 0){
+// 		photoIndex--;	
+// 	} //photoArray[photoIndex]
+// 	else{
+// 		photoIndex = photoArray.length - 1;
+// 	}
+// 	$(photoArray[photoIndex]).toggle();
+// }
+
+// function scrollForwardThroughImages(){
+// 	var photoArray = $('.cover-photo');
+// 	$(photoArray[photoIndex]).toggle();
+// 	if(photoIndex < photoArray.length - 1){
+// 		photoIndex++;
+// 	}
+// 	else {
+// 		photoIndex = 0;
+// 	}
+// 	$(photoArray[photoIndex]).toggle();
+
+// }
+
+// function hideImagesExceptFirstOne(){
+// 	var photoArray = $('.cover-photo');
+// 	if (photoArray.length > 1) {
+// 		for (i = 0; i < photoArray.length; i++){
+// 			if(i !== photoIndex){
+// 				$(photoArray[i]).toggle();
+// 			}
+// 		}		
+// 	}
+// }
+
+// *************************************
+
+var cityIndex = 0 
+function scrollBackThroughDisplays(){
+	var cityArray = $('.city-display');
+	$(cityArray[cityIndex]).toggle();
+	if(cityIndex > 0){
+		cityIndex--;	
+	} //photoArray[photoIndex]
 	else{
-		photoIndex = photoArray.length - 1;
+		cityIndex = cityArray.length - 1;
 	}
-	$(photoArray[photoIndex]).toggle();
+	$(cityArray[cityIndex]).toggle();
 }
 
-function scrollForwardThroughImages(){
-	var photoArray = $('.cover-photo');
-	$(photoArray[photoIndex]).toggle();
-	if(photoIndex < photoArray.length - 1){
-		photoIndex++;
+function scrollForwardThroughDisplays(){
+	var cityArray = $('.city-display');
+	$(cityArray[cityIndex]).toggle();
+	if(cityIndex < cityArray.length - 1){
+		cityIndex++;
 	}
 	else {
-		photoIndex = 0;
+		cityIndex = 0;
 	}
-	$(photoArray[photoIndex]).toggle();
+	$(cityArray[cityIndex]).toggle();
+
 }
 
-function hideImagesExceptFirstOne(){
-	var photoArray = $('.cover-photo');
-	if (photoArray.length > 1) {
-		for (i = 0; i < photoArray.length; i++){
-			if(i !== photoIndex){
-				$(photoArray[i]).toggle();
+
+function hideDisplaysExceptFirstOne(){
+	var cityArray = $('.city-display');
+	if (cityArray.length > 1) {
+		for (i = 0; i < cityArray.length; i++){
+			if(i !== cityIndex){
+				$(cityArray[i]).toggle();
 			}
 		}		
 	}
 }
+
+// *************************************
+
+
+
 	
 function show_sign_up_modal(){
 	$('#sign-up-modal').toggle();
