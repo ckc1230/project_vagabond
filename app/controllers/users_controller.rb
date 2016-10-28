ef8b9ddb589e00b3f23999cbfa93854e693ea250
nathanallen <thenathanator@gmail.com>
2016-10-28 13:55:19 -0700

    make controller helper methods private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	skip_before_action :verify_authenticity_token, only: [:validate_user]
- 
+
 	def show
 		@user = User.friendly.find(params[:id])
 		@posts = @user.posts.order('created_at DESC').paginate(:page => params[:page], :per_page => 3)
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 	def create
 
 		@user = User.new(user_params)
-		if @user.valid? 
+		if @user.valid?
 			@user.save
 			session[:user_id] = @user.id
 			redirect_to user_path(@user)
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   #     if @user.save
   #       # Tell the UserMailer to send a welcome email after save
   #       UserMailer.welcome_email(@user).deliver_now
- 
+
   #       format.html { redirect_to(@user, notice: 'User was successfully created.') }
   #       format.json { render json: @user, status: :created, location: @user }
   #     else
@@ -38,50 +38,50 @@ class UsersController < ApplicationController
   #     end
   #   end
 
-	
-	
+
+
 	def update
-		 user = current_user.update(user_params)
-		 redirect_to user_path
-	end
+     user = current_user.update(user_params)
+     redirect_to user_path
+  end
 
-
-	def validate_user
-		@user = User.new(user_params)
-		if @user.valid?
-			render :status => 200, nothing: true
-		else
-			render :status => 400, nothing: true
-		end
-	end
-
-	def get_cities_id
-		cities_id = [].to_set
-		@user.posts.each do |f|
-			if f.city_id
-				cities_id.add(f.city_id)
-			end
-		end
-		return cities_id
-	end
-	def get_cities(id_array)
-		cities = [].to_set
-		id_array.each do |f|
-			cities.add(City.find(f))
-		end
-		return cities
-	end
-
-	def count_occurences
-		count_occurences = []
-		@posted_cities_id.each do |f|
-			count_occurences << (Post.all.count(f))
-		end
-		return count_occurences
-
-	end
+  def validate_user
+    @user = User.new(user_params)
+    if @user.valid?
+      render :status => 200, nothing: true
+    else
+      render :status => 400, nothing: true
+    end
+  end
 
 	private
+
+  def get_cities_id
+    cities_id = [].to_set
+    @user.posts.each do |f|
+      if f.city_id
+        cities_id.add(f.city_id)
+      end
+    end
+    return cities_id
+  end
+  def get_cities(id_array)
+    cities = [].to_set
+    id_array.each do |f|
+      cities.add(City.find(f))
+    end
+    return cities
+  end
+
+  def count_occurences
+    count_occurences = []
+    @posted_cities_id.each do |f|
+      count_occurences << (Post.all.count(f))
+    end
+    return count_occurences
+
+  end
+
 	def user_params
 		params.require(:user).permit(:first_name, :last_name, :current_city, :email, :profile_image, :password, :photo)
 	end
