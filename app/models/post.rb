432d291c3391218b14f232b9c2505ff425356baa
nathanallen <thenathanator@gmail.com>
2016-10-28 14:05:32 -0700

    hot-fix: fix association of user and city with post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :users, optional: true
-  belongs_to :cities, optional: true
+  belongs_to :user, optional: true
+  belongs_to :city, optional: true
   validates :title, :post_text, :city_id, :user_id, presence: true
   validates_length_of :title, :minumum => 1, :maximum => 200
 end
